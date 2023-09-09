58a1599c1a60b9bcf475e7ec70448c025b0d6100
tope-olajide <31278728+tope-olajide@users.noreply.github.com>
2023-09-09 18:40:17 +0100

    Fix localStorage is not defined error

--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -1,16 +1,23 @@
-"use client";
-
+'use client'
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import NextAppDirEmotionCacheProvider from "./EmotionCache";
 import { darkTheme, lightTheme } from "./theme";
-import { ReactNode, createContext, useMemo, useState } from "react";
+import { ReactNode, createContext, useMemo, useState, useEffect } from "react";
 
 export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 export default function ThemeRegistry({ children }: { children: ReactNode }) {
-  const currentMode = localStorage.getItem("currentMode") || "dark";
-  const [mode, setMode] = useState<string>(currentMode);
+  const [mode, setMode] = useState<string>("dark");
+
+  // Use useEffect to get the theme mode from localStorage when the component mounts
+  useEffect(() => {
+    const currentMode = localStorage.getItem("currentMode");
+    if (currentMode) {
+      setMode(currentMode);
+    }
+  }, []);
+
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () => {
@@ -23,6 +30,7 @@ export default function ThemeRegistry({ children }: { children: ReactNode }) {
     }),
     []
   );
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <NextAppDirEmotionCacheProvider options={{ key: "mui" }}>
